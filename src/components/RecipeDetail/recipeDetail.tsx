f5eebce3697eaae75124e8c58e8d87a03b464072
jebj <jensbjark@proton.me>
2025-12-08 15:18:26 +0000

    vinna í forEach

--- src/components/RecipeDetail/recipeDetail.tsx
+++ src/components/RecipeDetail/recipeDetail.tsx
@@ -16,21 +16,26 @@ const recipeDetail = () => {
         const res = await fetch(`${url_Details} ${id}`);
         const data = await res.json();
         setRecipe(data.meals[0]);
-        setInstructions(data.meals[0].strInstructions);
         console.log(data.meals);
-
+        setInstructions(data.meals[0].strInstructions);
         Object.keys(data.meals[0]).forEach((key) => {
           if (key.includes("strIngredient") && data.meals[0][key] !== "") {
             setIngredients((prev) => {
-              if (prev.length === 0) return [data.meals[0][key]];
-              else return [...prev, data.meals[0][key]];
+              if (prev.length !== 0) {
+                return [...prev, data.meals[0][key]];
+              } else if (prev.length === 0) {
+                return [data.meals[0][key]];
+              }
             });
           }
 
           if (key.includes("strMeasure") && data.meals[0][key] !== "") {
             setMeasures((prev) => {
-              if (prev.length === 0) return [data.meals[0][key]];
-              else return [...prev, data.meals[0][key]];
+              if (prev.length === 0) {
+                return [data.meals[0][key]];
+              } else {
+                return [...prev, data.meals[0][key]];
+              }
             });
           }
         });
@@ -45,13 +50,13 @@ const recipeDetail = () => {
       <div className="wrapper">
         <h2>Uppskrift</h2>
         <div className="recipe">
-          <img src={recipe?.strMealThumb} />
+          <img className="img" src={recipe?.strMealThumb} />
           <div className="metaData">{recipe?.strMeal}</div>
           <h3>Hráefni og Mælingar</h3>
           <div className="ingredients">
-            {ingredients.map((ingredient, index) => (
+            {ingredients?.map((ingredient, i) => (
               <div key={ingredient} className="ingredient">
-                <span>{ingredient}</span> - <span>{measures[index]}</span>
+                <span>{ingredient}</span> - <span>{measures[i]}</span>
               </div>
             ))}
           </div>
